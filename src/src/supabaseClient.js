361ec8d630922a313926b7743182363a1b997e91
ojrv <ojrv.uy@gmail.com>
2025-10-06 13:21:44 -0300

    Delete src/src directory

--- src/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://fdlnptxefjzoxeiqsujx.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkbG5wdHhlZmp6b3hlaXFzdWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDE3ODIsImV4cCI6MjA3NDQxNzc4Mn0.BI9M80Fr-AevXnHBATTJZkRjFrCGn4x7cgDPkTinNms';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
